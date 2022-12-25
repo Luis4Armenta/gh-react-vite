342c00de9f9ee26cf95dc9ee93ab5897c2babd8e
Luis Angel Armenta Delgadillo <luigiangelxd@gmail.com>
2022-12-25 00:57:55 -0600

    Fix Bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://luis4armenta.github.io/',
+  base: 'https://luis4armenta.github.io/gh-react-vite/',
   plugins: [react()],
 })
